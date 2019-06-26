38717b69edfb8b99b0eaba133974d0af1e6a9fec
Heraklei0s <42822061+Heraklei0s@users.noreply.github.com>
2019-06-26 14:47:56 -0400

    Added GUI and message log

--- RogueLike/engine.py
+++ RogueLike/engine.py
@@ -5,6 +5,7 @@ from components.ai import BasicMonster
 from death_functions import kill_monster, kill_player
 from entity import Entity, get_blocking_entities_at_location
 from fov_functions import initialize_fov, recompute_fov
+from game_messages import MessageLog
 from game_states import GameStates
 from input_handlers import handle_keys
 from map_objects.game_map import GameMap
@@ -62,6 +63,8 @@ def main():
   fov_recompute = True
 
   fov_map = initialize_fov(game_map)
+
+  message_log = MessageLog(message_x, message_width, message_height)
   
   key = libtcod.Key()
   mouse = libtcod.Mouse()
@@ -69,12 +72,12 @@ def main():
   game_state = GameStates.PLAYERS_TURN
 
   while not libtcod.console_is_window_closed():
-    libtcod.sys_check_for_event(libtcod.EVENT_KEY_PRESS, key, mouse)
+    libtcod.sys_check_for_event(libtcod.EVENT_KEY_PRESS | libtcod.EVENT_MOUSE, key, mouse)
 
     if fov_recompute:
       recompute_fov(fov_map, player.x, player.y, fov_radius, fov_light_walls, fov_algorithm)
     
-    render_all(con, entities, player, game_map, fov_map, fov_recompute, screen_width, screen_height, bar_width, panel_height, panel_y, colors)
+    render_all(con, panel, entities, player, game_map, fov_map, fov_recompute, message_log, screen_width, screen_height, bar_width, panel_height, panel_y, mouse, colors)
 
     fov_recompute = False
     
@@ -118,7 +121,7 @@ def main():
       dead_entity = player_turn_result.get('dead')
 
       if message:
-        print(message)
+        message_log.add_message(message)
 
       if dead_entity:
         if dead_entity == player:
@@ -126,7 +129,7 @@ def main():
         else:
           message = kill_monster(dead_entity)
 
-        print(message)
+        message_log.add_message(message)
 
     if game_state == GameStates.ENEMY_TURN:
       for entity in entities:
@@ -138,7 +141,7 @@ def main():
             dead_entity = enemy_turn_result.get('dead')
 
             if message:
-              print(message)
+              message_log.add_message(message)
 
             if dead_entity:
               if dead_entity == player:
@@ -146,7 +149,7 @@ def main():
               else:
                 message = kill_monster(dead_entity)
 
-              print(message)
+              message_log.add_message(message)
 
               if game_state == GameStates.PLAYER_DEAD:
                 break
